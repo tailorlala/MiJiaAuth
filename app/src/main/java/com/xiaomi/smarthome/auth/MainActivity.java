24a69fa06e45baa322cf5bc3d16bbce45d486f4b
任磊 <renlei@xiaomi.com>
2017-06-06 14:34:04 +0800

    新增了错误的code值，新增了对绑定设备的授权

--- app/src/main/java/com/xiaomi/smarthome/auth/MainActivity.java
+++ app/src/main/java/com/xiaomi/smarthome/auth/MainActivity.java
@@ -1,7 +1,15 @@
 package com.xiaomi.smarthome.auth;
 
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.click).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                createFile();
+            }
+        });
+    }
+    private void createFile() {
+        String path = Environment.getExternalStorageDirectory().getPath() + "/smarthome_record/" + new SimpleDateFormat("yyyy_mm_dd").format(new Date())+"_____"+new SimpleDateFormat("HH_mm_dd").format(new Date())+"log.txt";
+        File file = new File(path);
+        try {
+
+            if (!file.getParentFile().exists()) {
+                file.getParentFile().mkdirs();
+            }
+            if (!file.exists()){
+                file.createNewFile();
+            }
+            FileOutputStream ous = new FileOutputStream(file);
+            if (ous != null){
+                Log.d("MainActivity","成功");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
--- authlib/src/main/java/com/xiaomi/smarthome/authlib/AuthCode.java
+++ authlib/src/main/java/com/xiaomi/smarthome/authlib/AuthCode.java
@@ -18,7 +18,11 @@ public class AuthCode {
     public static final int REQUEST_AUTH_ERROR = -107;//请求授权失败
     public static final int REQUEST_CODE_ERROR = -108;//请求的code错误
     public static final int REQUSET_DID_ERROR = -109;///缺少did
+    public static final int REQUEST_AUTH_NO_CAPABILITY = -110;///该设备不支持语音授权，或者该设备不属于你的名下
+    public static final int REQUEST_AUTH_NO_PERMISSION = -111;///该账号不支持该类型授权，请到开放平台申请
 
-    public static final int REQUEST_CODE_CALL_AUTH_FOR_APP = 1;//请求app授权
-    public static final int REQUEST_CODE_CALL_AUTH_FOR_DEVICE = 2;//请求设备授权
+
+    public static final int REQUEST_CODE_CALL_AUTH_FOR_APP = 4;//给应用授权
+    public static final int REQUEST_CODE_CALL_AUTH_FOR_DEVICE = 2;//给设备授权
+    public static final int REQUEST_CODE_CALL_AUTH_FOR_BIND_DEVICE = 6;//给需要绑定的设备授权
 }
